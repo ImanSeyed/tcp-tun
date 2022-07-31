7679ca15946441f9fd2a5ba702a5e8b3a5e9fbfd
Seyediman Seyedarab <imandevel@gmail.com>
2022-07-31 12:48:55 +0430

    use `sudo` if `doas` doesn't exist

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
+ROOTER=doas
+if ! command -v doas &> /dev/null; then
+	ROOTER=sudo
+fi
 make
-doas setcap cap_net_admin=eip ./tcp-tun
+$ROOTER setcap cap_net_admin=eip ./tcp-tun
 ./tcp-tun &
 pid=$!
-doas ip address add 192.168.1.1/24 dev tun0
-doas ip link set up dev tun0
+$ROOTER ip address add 192.168.1.1/24 dev tun0
+$ROOTER ip link set up dev tun0
 trap "kill $pid" INT
 wait $pid
