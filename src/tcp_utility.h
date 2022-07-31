31462a5093d68df403092fc72472cf071ff35f1f
Seyediman Seyedarab <imandevel@gmail.com>
2022-07-31 10:18:11 +0430

    fix fill_tcp_header arguments

--- src/tcp_utility.h
+++ src/tcp_utility.h
@@ -5,7 +5,7 @@
 #include "types.h"
 
 void parse_tcp_header(struct tcp_header *header, uint8_t *buffer, size_t start);
-void fill_tcp_header(struct tcp_header *header, union ipv4_addr src,
-		     union ipv4_addr dest, uint32_t seq_number,
+void fill_tcp_header(struct tcp_header *header, uint16_t src_port,
+		     uint16_t dest_port, uint32_t seq_number,
 		     uint16_t win_size);
 #endif
