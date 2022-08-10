3eb2bde36d3e109da1b63a25fb632adaad92135e
Seyediman Seyedarab <imandevel@gmail.com>
2022-08-10 21:12:36 +0430

    add CMakeLists instead of Makefile

--- Makefile
+++
@@ -1,9 +0,0 @@
-CC := gcc
-CFLAGS := -Wall -Wextra
-SOURCES := $(wildcard src/*.c src/*/*.c)
-INCLUDE_DIR := include/
-
-all:
-	$(CC) $(SOURCES) $(CFLAGS) -I $(INCLUDE_DIR) -o tcp-tun
-clean:
-	rm -rf tcp-tun
--- run.sh
+++ run.sh
@@ -2,11 +2,15 @@ ROOTER=doas
 if ! command -v doas &> /dev/null; then
 	ROOTER=sudo
 fi
-make
+
+if [ ! -d build ]; then
+	mkdir build
+fi
+cd build && cmake .. && make
 $ROOTER setcap cap_net_admin=eip ./tcp-tun
 ./tcp-tun &
 pid=$!
 $ROOTER ip address add 192.168.20.1/24 dev tun0
 $ROOTER ip link set up dev tun0
-trap "kill $pid" INT
+trap "kill $pid && cd .." INT
 wait $pid
