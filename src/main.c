1b1f0274ca907a1869a85569b80893cd75cfcb9f
Iman Seyed <ImanDevel@gmail.com>
2024-04-10 17:03:53 -0400

    rename variables for clarity

--- src/main.c
+++ src/main.c
@@ -11,56 +11,56 @@
 
 int main()
 {
-	int nic;
+	int nic_fd;
 	u8 buffer[1500];
-	struct ipv4_header input_ipv4_header;
-	struct tcp_header input_tcp_header;
+	struct ipv4_header incoming_ipv4h;
+	struct tcp_header incoming_tcph;
 	struct connections_hashmap *connections_ht;
 	struct TCB starter;
 	struct ifreq ifr = { 0 };
-	nic = tun_open("tun0", &ifr);
+	nic_fd = tun_open("tun0", &ifr);
 	connections_ht = connections_create();
 
-	union ipv4_addr ipv4, subnet;
-	init_ipv4_addr(&ipv4, 192, 168, 20, 1);
-	init_ipv4_addr(&subnet, 255, 255, 255, 0);
+	union ipv4_addr tun_ipv4, tun_subnet;
+	init_ipv4_addr(&tun_ipv4, 192, 168, 20, 1);
+	init_ipv4_addr(&tun_subnet, 255, 255, 255, 0);
 
-	tun_set_ip(nic, &ifr, &ipv4, &subnet);
+	tun_set_ip(nic_fd, &ifr, &tun_ipv4, &tun_subnet);
 
 	for (;;) {
-		read(nic, buffer, sizeof(buffer));
+		read(nic_fd, buffer, sizeof(buffer));
 
 		/* Ignore everything except IPv4 packets */
 		if (!(buffer[0] >= 0x45 && buffer[0] <= 0x4f))
 			continue;
 
-		ipv4h_from_buff(&input_ipv4_header, buffer, 0);
+		ipv4h_from_buff(&incoming_ipv4h, buffer, 0);
 
 		/* Ignore everything except TCP packets */
-		if (input_ipv4_header.protocol != TCP_PROTO)
+		if (incoming_ipv4h.protocol != TCP_PROTO)
 			continue;
 
-		tcph_from_buff(&input_tcp_header, buffer,
-			       ((input_ipv4_header.version_and_ihl.ihl) * 4));
+		tcph_from_buff(&incoming_tcph, buffer,
+			       ((incoming_ipv4h.version_and_ihl.ihl) * 4));
 
 		struct connection_quad new_quad;
-		new_quad.src.ip = input_ipv4_header.src_addr;
-		new_quad.dest.ip = input_ipv4_header.dest_addr;
-		new_quad.src.port = input_tcp_header.src_port;
-		new_quad.dest.port = input_tcp_header.dest_port;
+		new_quad.src.ip = incoming_ipv4h.src_addr;
+		new_quad.dest.ip = incoming_ipv4h.dest_addr;
+		new_quad.src.port = incoming_tcph.src_port;
+		new_quad.dest.port = incoming_tcph.dest_port;
 
 		u16 data_offset =
-			input_ipv4_header.total_length -
-			(((input_ipv4_header.version_and_ihl.ihl) +
-			  input_tcp_header.flags_and_data_offset.data_offset) *
+			incoming_ipv4h.total_length -
+			(((incoming_ipv4h.version_and_ihl.ihl) +
+			  incoming_tcph.flags_and_data_offset.data_offset) *
 			 4);
 
 		if (connections_entry_is_occupied(connections_ht, &new_quad)) {
-			on_packet(nic, &input_ipv4_header, &input_tcp_header,
+			on_packet(nic_fd, &incoming_ipv4h, &incoming_tcph,
 				  &starter, buffer + data_offset);
 		} else {
-			starter = accept_request(nic, &input_ipv4_header,
-						 &input_tcp_header);
+			starter = accept_request(nic_fd, &incoming_ipv4h,
+						 &incoming_tcph);
 			connections_set(connections_ht, &new_quad,
 					starter.state);
 		}
