af4b3ab9030573aefb16631760ab69d8cdac66b3
Iman Seyed <ImanDevel@gmail.com>
2024-05-23 00:17:47 -0400

    no need to handle SIGINT by secure_run.sh

--- secure_run.sh
+++ secure_run.sh
@@ -5,7 +5,4 @@ ROOTER=sudo
 set -x
 cmake -Bbuild/ && make -j$(nproc) -C build/ || exit 1
 $ROOTER setcap cap_net_admin=eip build/tcp-tun || exit 1
-build/tcp-tun &
-pid=$!
-trap "kill $pid" INT
-wait $pid
+build/tcp-tun
